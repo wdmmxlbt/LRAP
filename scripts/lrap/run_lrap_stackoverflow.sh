b67d5fa974c703052978252381414eae6d356e59
wdmmxlbt <2480860179@qq.com>
2025-07-24 02:52:39 +0000

    add run and scripts

---
+++ scripts/lrap/run_lrap_stackoverflow.sh
@@ -0,0 +1,35 @@
+
+#!/usr/bin bash
+
+method="lrap"
+prefix="outs"
+pretrain_suffix="lrap_pretrain"
+suffix="lrap"
+
+for dataset in "stackoverflow"
+do
+    for known_cls_ratio in 0.25 0.5 
+    do
+        for seed in 0
+        do 
+            python run.py \
+            --dataset $dataset \
+            --method ${method} \
+            --known_cls_ratio $known_cls_ratio \
+            --seed $seed \
+            --config_file_name "./methods/${method}/configs/config_${dataset}_lrap_finetune.yaml" \
+            --save_results \
+            --log_dir "./${prefix}/${method}/logs/${dataset}_${suffix}" \
+            --output_dir "./${prefix}/${method}" \
+            --dataset_dir "datasets_${known_cls_ratio}/data_${known_cls_ratio}_${dataset}_${seed}.pkl" \
+            --model_file_name "model_${known_cls_ratio}_${dataset}_${seed}_${suffix}.pt" \
+            --pretrained_nidmodel_file_name "best_epoch_bestmodel_${known_cls_ratio}_${dataset}_${seed}_${pretrain_suffix}.pt" \
+            --result_dir "./${prefix}/${method}/results_ds_v3_3level_100h_${dataset}_${suffix}_kcr${known_cls_ratio}" \
+            --results_file_name "results_${known_cls_ratio}_${dataset}_${seed}_${suffix}.csv"  \
+            --cl_loss_weight 1.0 \
+            --semi_cl_loss_weight 1.0
+        done
+    done
+done
+
+
